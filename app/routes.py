2c3e411d092dec70fca794f9dbebbc42cb2c1c64
Airan <airanthem666@gmail.com>
2020-02-25 13:10:39 +0800

    完成注册功能

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,5 @@
 from app import app
+from app import db
 from flask import render_template, flash, redirect, url_for, request
 from werkzeug.urls import url_parse
 
@@ -6,6 +7,9 @@ from werkzeug.urls import url_parse
 from app.forms import LoginForm
 from flask_login import current_user, login_user, logout_user, login_required
 from app.models import User
+
+# for register
+from app.forms import RegistrationForm
 
 @app.route('/')
 @app.route('/index')
@@ -44,3 +48,17 @@ def login():
 def logout():
     logout_user()
     return redirect(url_for('index'))
+
+@app.route('/register',methods=['GET','POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data, email=form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('Congratulations, register success')
+        return redirect(url_for('login'))
+    return render_template('register.html', title='Register', form=form)
